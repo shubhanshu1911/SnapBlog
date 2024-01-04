c8430a0b0bdc8fadf548bd41d5ab4cd02dc1bb67
shubhanshu1911 <shubhkhare19@gmail.com>
2024-01-04 11:36:00 +0530

    changed the port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
-const port = 3000; // this will be running the website 
+const port = process.env.PORT || 3000; // this will be running the website 
 const API_URL = "http://localhost:4000";
 
 app.use(express.static("public"));
